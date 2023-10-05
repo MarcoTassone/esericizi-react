994389bf8aecfdce26a3ff2d4582451284ed7712
Marco <tassonemarco67@gmail.com>
2023-10-05 19:48:09 +0200

    added 9-components exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,11 +1,15 @@
+import { Hello } from "./components/Hello"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+      <Hello />
+    </div>
   )
 }
 
 export default App
+
+// non ho capito per quale motivo non mi esce in pagina, eppure mi sembra di aver fatto tutto giusto 
+// Hello.jsx l'ho creato giusto per vedere se usciva qualcosa
