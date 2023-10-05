7ff00f1f014b511e7dd77819983ec1ca7e4f8f0c
Marco <tassonemarco67@gmail.com>
2023-10-05 15:45:28 +0200

    added 4-jsx exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-      
+      {/* se la variabile un elemento JSX invece di una stringa, React lo tratterà come un child component e proverà a fare il rendering*/}
     </>
   )
 }
