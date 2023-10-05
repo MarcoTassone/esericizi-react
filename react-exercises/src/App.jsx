926d88db9a74af1b3470463d87d14f3b948c50fc
Marco <tassonemarco67@gmail.com>
2023-10-05 21:38:02 +0200

    added 13-components exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,11 +1,17 @@
+import { Hello } from "./components/Hello"
 
 function App() {
 
   return (
     <>
-      
+      <Hello/>
+      <Hello/>
+      <Hello/>
+      <Hello/>
     </>
   )
 }
 
 export default App
+
+// sì è possibile usare il componente Hello più volte e succede che viene ripetuto il codice
