d0e33653b558c038554384faa614724fd243d119
Marco <tassonemarco67@gmail.com>
2023-10-05 21:44:37 +0200

    added 14-components exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,11 +1,16 @@
+import { Hello } from "./components/Hello"
+import { Message } from "./components/Message"
 
 function App() {
 
   return (
     <>
-      
+      <Hello />
+      <Message />
     </>
   )
 }
 
 export default App
+
+// sì si può importare Message direttamente su App e succede la stessa cosa di quando lo si renderizza su Hello
---
+++ react-exercises/src/components/Hello.jsx
@@ -0,0 +1,8 @@
+
+export function Hello(){
+    return (
+        <div>
+            <h2>Hello World!</h2>
+        </div>
+    )
+}
