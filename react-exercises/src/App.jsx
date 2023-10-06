38128feb916e1a4ed250ae3871b8db38b0992fb2
Marco <tassonemarco67@gmail.com>
2023-10-06 20:57:08 +0200

    added 30-state exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -3,7 +3,8 @@ function App() {
 
   return (
     <>
-      
+      {/* è meglio passare una funzione come parametro perché, essendo lo useState una funzione asincrona, potrebbero esserci degli errori
+      nell'aggiornamento dell'ultimo stato */}
     </>
   )
 }
