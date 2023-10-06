d7090cf730defd5f0ad659ab39113321e2702036
Marco <tassonemarco67@gmail.com>
2023-10-06 19:54:04 +0200

    added 25-conditional-rendering exercise

---
+++ react-exercises/src/components/Welcome.jsx
@@ -0,0 +1,10 @@
+import { Age } from "./Age"
+
+export function Welcome({name, age}){
+    return (
+    <>
+        <p>Welcome, {name}</p>
+        {age > 18 ? <Age age={age}/> : <p>You are very young!</p>}
+    </>
+    )
+}
