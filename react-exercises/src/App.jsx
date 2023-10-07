90835dc2a6c7cc4a54f0ef12156ab00086f0e265
Marco <tassonemarco67@gmail.com>
2023-10-07 16:54:30 +0200

    added 35-useEffect exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,9 +1,13 @@
+import { Clock } from "./Clock"
+import { Counter } from "./Counter"
 
 function App() {
 
   return (
     <>
-      
+      <Counter initialValue={0} incrementAmount={1}/>
+
+      <Clock />
     </>
   )
 }
