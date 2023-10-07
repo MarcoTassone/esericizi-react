e8e057b11d65e22cc40cde9b4dd7f33c762854e4
Marco <tassonemarco67@gmail.com>
2023-10-07 15:55:39 +0200

    added 33-state exercise

---
+++ react-exercises/src/Counter.jsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+import { CounterDisplay } from "./CounterDisplay";
+
+export function Counter({initialValue, incrementAmount}){
+    const [counter, setCount] = useState(initialValue);
+
+    const increment = () => {
+        setCount((prev) => prev + incrementAmount)
+    }
+
+    const decrement = () => {
+        setCount((prev) => prev - incrementAmount)
+    }
+
+    const reset = () => {
+        setCount(initialValue)
+    }
+
+    return(
+        <div>
+            <CounterDisplay counter={counter}/>
+            <button onClick={increment}>Increase</button>
+            <button onClick={decrement}>Decrease</button>
+            <button onClick={reset}>Reset</button>
+        </div>
+    )
+}
