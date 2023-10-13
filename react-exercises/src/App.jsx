924c3105ff9edc18bca273e7fdc30631d173f8e5
Marco <tassonemarco67@gmail.com>
2023-10-13 13:01:48 +0200

    added 54-lists exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,9 +1,16 @@
+import { Colors } from "./components/Colors"
 
 function App() {
 
   return (
     <>
-      
+      <Colors items={[
+        {id: 1, name: 'white'},
+        {id: 2, name: 'black'},
+        {id: 3, name: 'brown'},
+        {id: 4, name: 'purple'},
+        {id: 5, name: 'pink'}
+      ]}/>
     </>
   )
 }
---
+++ react-exercises/src/components/Colors.jsx
@@ -0,0 +1,10 @@
+export function Colors({ items }){
+
+    return(
+        <ul>
+            {items.map((color) => 
+            (<li key={color.id}>{color.name}</li>)
+            )}
+        </ul>
+    )
+}
