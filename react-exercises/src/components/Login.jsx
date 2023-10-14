04ce5a912034e2595fac538120439d12ec17701a
Marco <tassonemarco67@gmail.com>
2023-10-15 00:11:24 +0200

    added 62-styling-components exercise

---
+++ react-exercises/src/components/Login.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export function Login(){
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+
+    function handleUsernameInputChange(e){
+        setUsername(e.target.value);
+    }
+    
+    function handlePasswordInputChange(e){
+        setPassword(e.target.value);
+    }
+
+    function onLogin(){
+        if (username && password) {
+            setUsername(username)
+            setPassword(password)
+        }
+    }
+
+    function resetState(){
+        setUsername('')
+        setPassword('')
+    }
+
+    return (
+        <>
+            <form onSubmit={onLogin}>
+                <label>Username:</label>
+                <input type="text" name="username" onChange={handleUsernameInputChange}/>
+                <label>Password:</label>
+                <input type="password" name="password" onChange={handlePasswordInputChange}/>
+                <button style={{
+                     backgroundColor: password.length < 8 ?'red' : 'green',
+                }} disabled={!username || !password}>Login</button>
+                <button onClick={resetState}>Reset</button>  
+                 {/*lascio il reset giusto per "dimostrare" che grazie al preventDefault il reset non funziona */}
+            </form>
+        </>
+        )
+}
