6d5e84f2a1e23e77e0fff682c5e83bc6cd50942a
Marco <tassonemarco67@gmail.com>
2023-10-21 17:08:41 +0200

    added 73-datafetching-with-fetch-and-useeffect exercise

---
+++ react-exercises/src/components/GithubUser.jsx
@@ -0,0 +1,30 @@
+import { useEffect, useState } from "react";
+
+export function GithubUser({username}){
+    const [data, setData] = useState(null)
+    
+    async function fetchGithubUsers(username){
+        try {
+            const result = await fetch(`https://api.github.com/users/${username}`);
+            const json = await result.json();
+            setData(json)
+            console.log(json);
+        } catch (error) {
+            console.error(error);
+            setData(null)
+        }  
+    }
+
+    useEffect(() => {
+        fetchGithubUsers(username)
+    }, [username]);
+
+    return(
+        <div>
+            {data && <h1>{data.login}</h1>}
+            {data && <img src={data.avatar_url}/>}
+            {data && <h1>{data.name}</h1>}
+            {/* purtroppo il name non viene mostrato perché è null */}
+        </div>
+    )
+}
