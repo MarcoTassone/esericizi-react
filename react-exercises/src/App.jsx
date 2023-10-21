7a340496f0eff0eb3511f7c17d76b80a9fb11398
Marco <tassonemarco67@gmail.com>
2023-10-21 14:43:58 +0200

    added 72-context exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,9 +1,26 @@
+import { useState } from "react"
+import { Clock } from './components/Clock'
+import { LanguageContext } from "./components/LanguageContext"
 
 function App() {
 
+  const [language, setLanguage] = useState('en')
+
+  function handleSetLanguage(language){
+    setLanguage(language)
+  }
+
   return (
     <>
-      
+      <LanguageContext value={language}>
+        <select onChange={handleSetLanguage}>
+          <option value='en'>EN</option>
+          <option value='it'>IT</option>
+        </select>
+        <Clock/>
+      </LanguageContext>
+
+      {/* togliendo il context provider in console esce un errore che dice che il context consumer si aspetta un singolo child che è una funzione e non dei children */}
     </>
   )
 }
