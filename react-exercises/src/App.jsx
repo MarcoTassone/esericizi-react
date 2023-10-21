04721f37d8c69cee7dcfc1692993b82b4e163ee2
Marco <tassonemarco67@gmail.com>
2023-10-21 13:11:24 +0200

    added 70-context exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from "react"
+import { Clock } from "./components/Clock"
+import { LanguageContext } from "./components/LanguageContext"
 
 function App() {
 
+  const [language, setLanguage] = useState('en')
+
+  function handleSetLanguage(language){
+    setLanguage(language)
+  }
+
   return (
     <>
-      
+      <LanguageContext.Provider value={language}>
+        <select onChange={handleSetLanguage}>
+          <option value='en'>EN</option>
+          <option value='it'>IT</option>
+        </select>
+        <Clock/>
+      </LanguageContext.Provider>
     </>
   )
 }
---
+++ react-exercises/src/components/Clock.jsx
@@ -0,0 +1,25 @@
+import { useContext, useEffect, useState } from "react"
+import { LanguageContext } from "./LanguageContext";
+
+export function Clock(){
+
+    const language = useContext(LanguageContext)
+
+    const [date, setDate] = useState(new Date());
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setDate(new Date())
+        }, 1000)
+
+        return () => {
+            clearInterval(intervalId)
+        }
+    }, [])
+
+    return(
+        <div>
+            <h2>{language === 'en' ? 'Current time:' : 'Orario attuale:'}{date.toLocaleTimeString()}</h2>
+        </div>
+    )
+}
