1d74f74e3125bf5d401af5f2a3812a1b3dd8c5eb
Marco <tassonemarco67@gmail.com>
2023-10-24 20:09:16 +0200

    added 82-react-router exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,10 +1,12 @@
+import { Route, Routes } from "react-router-dom"
+import { Welcome } from "./components/Welcome"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <Routes>
+      <Route path="/" element={<Welcome name="Marco"/>}/>
+    </Routes>
   )
 }
 
