8a459e5f125d7510594a78751eab058dc74f4c15
Marco <tassonemarco67@gmail.com>
2023-10-25 18:11:38 +0200

    added 85-react-router exercise

--- react-exercises/src/App.jsx
+++ react-exercises/src/App.jsx
@@ -1,10 +1,23 @@
+import { Link, Route, Routes } from "react-router-dom"
+import { Counter } from "./components/Counter"
+import { ShowGithubUser } from "./components/ShowGithubUser"
+import { Welcome } from "./components/Welcome"
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div>
+
+      <div>
+        <Link to="/">Home</Link> | <Link to="/counter">Counter</Link> | <Link to="users/MarcoTassone">Github User</Link>
+      </div>
+    
+    <Routes>
+      <Route path="/" element={<Welcome name="Marco"/>}/>
+      <Route path="/counter" element={<Counter initialValue={0} incrementAmount={1}/>}/>
+      <Route path="users/:username" element={<ShowGithubUser/>}/>
+    </Routes>
+    </div>
   )
 }
 
